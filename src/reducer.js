3fb277de077ffad1c1f449a4baa089786f230995
matomugo60 <matomugo60@gmail.com>
2023-03-21 13:01:57 +0300

    Complete

--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,22 @@
+let state = { count: 0 }
+
 function changeState(state, action) {
   switch (action.type) {
-    case "counter/increment":
-      return { count: state.count + 1 };
+    case 'counter/increment':
+      return { count: state.count + 1 }
     default:
-      return state;
+      return state
   }
 }
 
-let state = { count: 0 };
-let action = { type: "counter/increment" };
+function render() {
+  document.body.textContent = state.count
+}
 
-changeState(state, action);
+function dispatch(action) {
+  state = changeState(state, action)
+  render()
+}
+
+dispatch({ type: 'counter/increment' })
+dispatch({ type: 'counter/increment' })
